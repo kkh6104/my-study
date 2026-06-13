eee85496ad665bdcc6a633e4cf0d21c68d34c2ff
kkh6104 <kkh6104@gmail.com>
2026-06-13 17:54:05 +0900

    코드업 자습 자바

---
+++ CodeUp2/src/codeup/q1094/Main.java
@@ -0,0 +1,27 @@
+package codeup.q1094;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] randomN = new int[n]; 
+		
+		for (int i = 0 ; i < n ; i++ ) {
+			randomN[(n - 1 - i)] = sc.nextInt();
+						
+		}
+		
+		for (int i = 0 ; i < n ; i++ ) {
+			System.out.printf("%d ", randomN[i]);
+		}
+		
+		
+		sc.close();
+		
+	}
+
+}
---
+++ CodeUp2/src/codeup/q1099/Main.java
@@ -0,0 +1,66 @@
+package codeup.q1099;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int[][] board = new int[10][10];
+		
+		for(int i = 0 ; i < 10 ; i++) {
+			for(int j = 0 ; j < 10 ; j++) {
+				board[i][j] = sc.nextInt();
+			}
+		}
+		
+		int x = 1;
+
+		Loop1 :
+		
+		for(int y =1 ; y < 9 ; y++) {
+		
+				switch(board[y][x]) {
+				case 2 : {
+					board[y][x] = 9;
+					break Loop1 ;
+					}
+				case 0 : {
+					board[y][x] = 9;
+					x++;
+					y--;
+					break;
+					}
+				case 1 : {
+					switch(board[y+1][x-1]) {
+						case 2 : {
+							board[y+1][x-1] = 9;
+							break Loop1;
+							}
+						case 1 : {
+							break Loop1;
+							}
+						case 0 : {
+							board[y+1][x-1] = 9;
+							break;
+							}
+						}
+					}
+				}
+			
+			
+		}
+		
+		for(int i = 0 ; i < 10 ; i++) {
+			for(int j = 0 ; j < 10 ; j++) {
+				System.out.printf("%d ", board[i][j]);
+			}
+			System.out.println();
+		}
+		
+		sc.close();
+		
+	}
+}
