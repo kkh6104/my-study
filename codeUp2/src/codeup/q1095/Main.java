5b064b695092dc404dc50b006c5bd9bf3689cd3d
kkh6104 <kkh6104@gmail.com>
2026-06-13 17:52:07 +0900

    코드업 자바 독학

---
+++ codeUp2/src/codeup/q1095/Main.java
@@ -0,0 +1,26 @@
+package codeup.q1095;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int call;
+		int minN = 23;
+		
+		for (int i = 0 ; i < n ; i++ ) {
+			call = sc.nextInt();
+			if (call < minN) minN = call;
+						
+		}
+		
+		System.out.print(minN);
+		
+		sc.close();
+		
+	}
+
+}
