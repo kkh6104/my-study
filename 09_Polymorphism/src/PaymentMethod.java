1802435788634814dbd5ed0936fe67ccaa525a43
kkh6104 <kkh6104@gmail.com>
2026-06-22 17:17:52 +0900

    26/06/22 수업

---
+++ 09_Polymorphism/src/PaymentMethod.java
@@ -0,0 +1,53 @@
+
+
+// 공통되는 결제 정보를 관리하기 위한 클래스
+// - 결제명(payName), 결제금액(payAmount), 결제 정보 출력 (processPay())
+
+public class PaymentMethod {
+
+	private String payName;  	
+	private int payAmount;
+	
+	public PaymentMethod() {
+		super();
+	}
+	
+	
+	public PaymentMethod(String payName, int payAmount) {
+		super();
+		this.payName = payName;
+		this.payAmount = payAmount;
+	}
+
+
+
+	public String getPayName() {
+		return payName;
+	}
+
+
+
+	public void setPayName(String payName) {
+		this.payName = payName;
+	}
+
+
+
+	public int getPayAmount() {
+		return payAmount;
+	}
+
+
+
+	public void setPayAmount(int payAmount) {
+		this.payAmount = payAmount;
+	}
+
+
+
+	public void processPay() {
+		System.out.println("=== 결제 정보 ===");
+		System.out.println("- 결제명: " +payName);
+		System.out.println("- 결제금액: " +payAmount);
+	}
+}
